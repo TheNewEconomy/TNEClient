96ab11f3e9c0497b3446f3ad6f9cbb48785cb3b6
Daniel V <daniel.viddy@gmail.com>
2018-06-26 15:22:53 -0400

    Minor changes.

--- src/main/java/com/creatorfromhell/tnemod/registry/RegistryHandler.java
+++ src/main/java/com/creatorfromhell/tnemod/registry/RegistryHandler.java
@@ -1,6 +1,7 @@
 package com.creatorfromhell.tnemod.registry;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -20,5 +21,10 @@ public class RegistryHandler {
   @SubscribeEvent
   public static void registerItems(RegistryEvent.Register<Item> event) {
     ItemsRegistry.register(event.getRegistry());
+  }
+
+  @SubscribeEvent
+  public static void registerItems(ModelRegistryEvent event) {
+    ItemsRegistry.registerModels();
   }
 }
